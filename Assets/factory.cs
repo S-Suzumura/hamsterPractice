d49e579a819624bfca7bbb1a8c8296eeef809927
S-Suzumura <s.suzumura21@gmail.com>
2019-12-11 12:13:56 +0900

    クリックするとハムスターが増える

---
+++ Assets/factory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class factory : MonoBehaviour
+{
+public GameObject ham;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            Instantiate(ham,new Vector3(0,0,-2),Quaternion.identity);
+	    }	
+    }
+}
---
+++ Assets/ham.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ham : MonoBehaviour{
+
+    private Rigidbody2D rb = null;
+    public float directionX = 0;
+    public float directionY = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        while(directionX==0||directionY==0){
+        directionX = Random.Range(-1,2);
+        directionY = Random.Range(-1,2);
+        }
+        rb.velocity = new Vector2(directionX, directionY);
+    }
+
+    void Update()
+    {
+    }
+    void OnCollisionEnter2D(Collision2D coll){
+        int temp = Random.Range(0,3);
+        if(temp==0){
+        directionX *= -1;
+        } else if(temp==1){
+        directionY *= -1;
+        } else {
+        directionX *= -1;
+        directionY *= -1;
+        }
+        rb.velocity = new Vector2(directionX, directionY);
+    }
+
+}
